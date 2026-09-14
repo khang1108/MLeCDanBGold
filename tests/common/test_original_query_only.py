6e5ea4c3f6535f090e8d054ac3f4e0b1a5fe243c
Nguyen Phuc Khang <nguyenphuc.khang110806@gmail.com>
2026-09-14 12:20:11 +0700

    merge: new features for vbs

--- tests/common/test_original_query_only.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-import importlib.util
-
-
-def test_common_schema_package_is_absent() -> None:
-    """Keep the retired generic schema package undiscoverable."""
-
-    assert importlib.util.find_spec("hcmai.common.schemas") is None
--- tests/unit/temporal/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Package marker keeping unit temporal test modules uniquely importable."""
